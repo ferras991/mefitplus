688d71394059dc85a2f22fdbacac36b9bc8526d1
ferras991 <ferrasdocas@gmail.com>
2020-01-13 20:10:37 +0000

    Organize the code. Add comments to the code.

--- app/src/main/java/com/example/exercicio3_imc/UserInfo.java
+++ app/src/main/java/com/example/exercicio3_imc/UserInfo.java
@@ -8,14 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.exercicio3_imc.Class.User;
 import com.example.exercicio3_imc.Globals.Globals;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class UserInfo extends AppCompatActivity {
 
     private EditText name, birthDate, gender, height, email;
-    private Button changeBtn;
+    private Button changeUserInfoBtn;
 
     private FirebaseAuth mAuth;
 
@@ -33,14 +32,16 @@ public class UserInfo extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        //EditTexts
         name = findViewById(R.id.nameFieldUserInfo);
         birthDate = findViewById(R.id.birthDateFieldUserInfo);
         gender = findViewById(R.id.genderFieldUserInfo);
         height = findViewById(R.id.heightFieldUserInfo);
         email = findViewById(R.id.emailFieldUserInfo);
-        changeBtn = findViewById(R.id.changePasswordUserInfo);
 
-//        User user = new User();
+        //Buttons
+        changeUserInfoBtn = findViewById(R.id.changeUserInfo);
+
         name.setText(Globals.name);
         birthDate.setText(Globals.birthDate);
         if (Globals.gender == 0) gender.setText(getResources().getString(R.string.genderFemale)); else gender.setText(getResources().getString(R.string.genderMale));
@@ -52,8 +53,42 @@ public class UserInfo extends AppCompatActivity {
     public void onClickUserInfo(View view) {
         switch (view.getId()) {
             case R.id.changePasswordUserInfo:
-                startActivity(new Intent(UserInfo.this, ChangePasswordActivity.class));
+                startActivity(new Intent(
+                        UserInfo.this,
+                        ChangeEmailPasswordActivity.class)
+                        .putExtra("change", "password"));
                 break;
+            case R.id.changeUserEmailInfo:
+                startActivity(new Intent(
+                        UserInfo.this,
+                        ChangeEmailPasswordActivity.class)
+                        .putExtra("change", "email"));
+                break;
+            case R.id.changeUserInfo:
+                if (changeUserInfoBtn.getText().toString().equals("Edit Fields")) {
+                    enableFields(true);
+                } else {
+                    enableFields(false);
+                }
+                break;
+        }
+    }
+
+    private void enableFields(boolean enable) {
+        if (enable) {
+            name.setEnabled(true);
+            birthDate.setEnabled(true);
+            gender.setEnabled(true);
+            height.setEnabled(true);
+
+            changeUserInfoBtn.setText("Save Fields");
+        } else {
+            name.setEnabled(false);
+            birthDate.setEnabled(false);
+            gender.setEnabled(false);
+            height.setEnabled(false);
+
+            changeUserInfoBtn.setText("Edit Fields");
         }
     }
 }
